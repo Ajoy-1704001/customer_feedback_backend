f1d6fce3094125bf1cced99660485b1a4aba1832
Ajoy Deb Nath <57573642+Ajoy-1704001@users.noreply.github.com>
2025-05-12 21:59:26 +0600

    added global exception handler

---
+++ src/main/java/com/deb/customer_feedback_backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.deb.customer_feedback_backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import com.deb.customer_feedback_backend.dto.MessageResponse;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(exception = UsernameNotFoundException.class)
+	public ResponseEntity<?> handleUsernameNotFoundException(UsernameNotFoundException ex, WebRequest request){
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageResponse("User not found"));
+	}
+	
+	@ExceptionHandler(exception = BadCredentialsException.class)
+	public ResponseEntity<?> handleBadCredentialsException(BadCredentialsException ex, WebRequest request){
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageResponse("Email/Password credential not matched"));
+	}
+}
